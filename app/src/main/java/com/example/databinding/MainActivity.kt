78c2dbaf6181e20becc90d6a50ab0c66855a1649
tatsuya.sakamoto <tatsuya.sakamoto@JMPC-154.local>
2022-01-24 18:33:00 +0900

    データバインディングを使用してTextView表示

---
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupDataBinding()
+    }
+
+    private fun setupDataBinding() {
+        // アクティビティのコンテンツビューを指定されたレイアウトに設定し、バインディングを返す
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(
+            this, R.layout.activity_main
+        )
+        // ViewModelのインスタンスを作成し、xmlファイルで決めた名前に渡す
+        binding.viewModel = ViewModel()
+    }
+
+}
+
+class ViewModel {
+    var name = "たっつぁん"
+}
+
